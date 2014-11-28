39b6bc41642a6d2ee15e08bf08a3d6456c51adbb
Matthew Pennell <github@matthewpennell.com>
2014-11-28 11:09:35 +0000

    Make filters work again.

--- public/j/eth.js
+++ public/j/eth.js
@@ -1,11 +1,17 @@
 $(document).ready(function () {
 
+    // Clicking an item in the table loads the details via Ajax.
     $('.losses a').click(function () {
         $('.details').html('<img src="/i/loader.gif" class="loader">');
         $.get(this.href, function (data) {
             $('.details').html(data);
         });
         return false;
+    });
+
+    // Clicking on filter checkboxes submits the form to reload the page.
+    $('.filters input').click(function () {
+        $('.filters')[0].submit();
     });
 
 });
