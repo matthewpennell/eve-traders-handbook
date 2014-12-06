b15e8629e24585df472b46b35c26af42e2e0bfb7
Matthew Pennell <github@matthewpennell.com>
2014-12-06 15:02:53 +0000

    Fix layout of page.

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -4,59 +4,63 @@
 
     <div class="background">
 
-        <h2>Losses in selected system(s)</h2>
+        <div class="main">
 
-        <table class="losses">
-            <thead>
-                <tr>
-                    <th class="num">Qty</th>
-                    <th>Type</th>
-                    <th>Category</th>
-                    <th>Meta</th>
-                </th>
-            </thead>
-            <tfoot>
-                <tr>
-                    <td colspan="4">
-                        @if ($page > 2)
-                            <a href="/?{{ $filter_url }}" class="ffwd">&#9666;&#9666;</a>
-                        @endif
-                        @if ($page > 1)
-                            <a href="/?page={{ $page - 1 }}{{ $filter_url }}">&#9666;</a>
-                        @endif
-                        Page {{ $page }} of {{ ceil($pages) }}
-                        @if ($page <= ceil($pages) - 1)
-                            <a href="/?page={{ $page + 1 }}{{ $filter_url }}">&#9656;</a>
-                        @endif
-                        @if ($page <= ceil($pages) - 2)
-                            <a href="/?page={{ ceil($pages) }}{{ $filter_url }}" class="ffwd">&#9656;&#9656;</a>
-                        @endif
-                    </td>
-                </tr>
-            </tfoot>
-            <tbody>
-                @foreach($items as $item)
+            <h2>Losses in selected system(s)</h2>
+
+            <table class="losses">
+                <thead>
                     <tr>
-                        <td class="num">
-                            @if ($item->allowManufacture)
-                                <img src="/eve/items/BPO.png" class="industry" title="This item can be manufactured by players" alt="Blueprint">
+                        <th class="num">Qty</th>
+                        <th>Type</th>
+                        <th>Category</th>
+                        <th>Meta</th>
+                    </th>
+                </thead>
+                <tfoot>
+                    <tr>
+                        <td colspan="4">
+                            @if ($page > 2)
+                                <a href="/?{{ $filter_url }}" class="ffwd">&#9666;&#9666;</a>
                             @endif
-                            {{ number_format($item->qty) }}</td>
-                        <td class="t{{ $item->typeID }}">
-                            <a href="/details/{{ $item->typeID }}">{{ $item->typeName }}</a>
-                            @if ($item->profitIndustry || $item->profitImport)
-                                <?php $profit = ($item->profitIndustry > $item->profitImport) ? $item->profitIndustry : $item->profitImport; ?>
-                                <span class="{{ ($profit > 0) ? 'profit' : 'loss' }} {{ ($item->profitIndustry > $item->profitImport) ? 'local' : 'import' }}">{{ number_format(round($profit)) }}</span>
+                            @if ($page > 1)
+                                <a href="/?page={{ $page - 1 }}{{ $filter_url }}">&#9666;</a>
+                            @endif
+                            Page {{ $page }} of {{ ceil($pages) }}
+                            @if ($page <= ceil($pages) - 1)
+                                <a href="/?page={{ $page + 1 }}{{ $filter_url }}">&#9656;</a>
+                            @endif
+                            @if ($page <= ceil($pages) - 2)
+                                <a href="/?page={{ ceil($pages) }}{{ $filter_url }}" class="ffwd">&#9656;&#9656;</a>
                             @endif
                         </td>
-                        <td>{{ $item->categoryName }}</td>
-                        <td>{{ $item->metaGroupName }}</td>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </tfoot>
+                <tbody>
+                    @foreach($items as $item)
+                        <tr>
+                            <td class="num">
+                                @if ($item->allowManufacture)
+                                    <img src="/eve/items/BPO.png" class="industry" title="This item can be manufactured by players" alt="Blueprint">
+                                @endif
+                                {{ number_format($item->qty) }}</td>
+                            <td class="t{{ $item->typeID }}">
+                                <a href="/details/{{ $item->typeID }}">{{ $item->typeName }}</a>
+                                @if ($item->profitIndustry || $item->profitImport)
+                                    <?php $profit = ($item->profitIndustry > $item->profitImport) ? $item->profitIndustry : $item->profitImport; ?>
+                                    <span class="{{ ($profit > 0) ? 'profit' : 'loss' }} {{ ($item->profitIndustry > $item->profitImport) ? 'local' : 'import' }}">{{ number_format(round($profit)) }}</span>
+                                @endif
+                            </td>
+                            <td>{{ $item->categoryName }}</td>
+                            <td>{{ $item->metaGroupName }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
 
-        <div class="details"></div>
+            <div class="details"></div>
+
+        </div>
 
         <?php echo $sidebar; ?>
 
