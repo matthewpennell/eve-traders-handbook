d4369904f15567037cc9b55c5029a0931957b910
Matthew Pennell <github@matthewpennell.com>
2015-07-12 21:23:23 +0100

    Add handling for empty default filters.

--- app/models/Item.php
+++ app/models/Item.php
@@ -16,20 +16,38 @@ class Item extends Eloquent {
 
     public static function selectedItems($page = 1, $whereraw = NULL, $per_page)
     {
-        return DB::table('items')
-            ->leftJoin('profits', 'items.typeID', '=', 'profits.typeID')
-            ->select('items.typeID', 'typeName', 'categoryName', 'metaGroupName', 'allowManufacture', 'manufactureCost', 'profitIndustry', 'profitImport', DB::raw('SUM(qty) AS qty'))
-            ->whereRaw('(' . implode(') and (', $whereraw) . ')')
-            ->orderBy('qty', 'desc')
-            ->groupBy('typeID')
-            ->skip(($page - 1) * $per_page)
-            ->take($per_page)
-            ->get();
+        if ($whereraw != NULL)
+        {
+            return DB::table('items')
+                ->leftJoin('profits', 'items.typeID', '=', 'profits.typeID')
+                ->select('items.typeID', 'typeName', 'categoryName', 'metaGroupName', 'allowManufacture', 'manufactureCost', 'profitIndustry', 'profitImport', DB::raw('SUM(qty) AS qty'))
+                ->whereRaw('WHERE (' . implode(') and (', $whereraw) . ')')
+                ->groupBy('typeID')
+                ->orderBy('qty', 'desc')
+                ->skip(($page - 1) * $per_page)
+                ->take($per_page)
+                ->get();
+        }
+        else {
+            return DB::table('items')
+                ->leftJoin('profits', 'items.typeID', '=', 'profits.typeID')
+                ->select('items.typeID', 'typeName', 'categoryName', 'metaGroupName', 'allowManufacture', 'manufactureCost', 'profitIndustry', 'profitImport', DB::raw('SUM(qty) AS qty'))
+                ->groupBy('typeID')
+                ->orderBy('qty', 'desc')
+                ->skip(($page - 1) * $per_page)
+                ->take($per_page)
+                ->get();
+        }
     }
 
     public static function getRowCount($whereraw = NULL)
     {
-        $query = DB::select(DB::raw('SELECT COUNT(*) AS count FROM (SELECT typeID, SUM(qty) FROM items WHERE (' . implode(') and (', $whereraw) . ') GROUP BY typeID) AS custom'));
+        $whererawsql = '';
+        if ($whereraw != NULL)
+        {
+            $whererawsql = 'WHERE (' . implode(') and (', $whereraw) . ')';
+        }
+        $query = DB::select(DB::raw('SELECT COUNT(*) AS count FROM (SELECT typeID, SUM(qty) FROM items ' . $whererawsql . ' GROUP BY typeID) AS custom'));
         return $query[0]->count;
     }
 
