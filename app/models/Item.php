8d27c9f67023d82c6d58436597556ecf43532a01
Matthew Pennell <github@matthewpennell.com>
2015-07-12 21:41:53 +0100

    Add more robust error handling.

--- app/models/Item.php
+++ app/models/Item.php
@@ -16,12 +16,12 @@ class Item extends Eloquent {
 
     public static function selectedItems($page = 1, $whereraw = NULL, $per_page)
     {
-        if ($whereraw != NULL)
+        if (is_array($whereraw) && count($whereraw) > 0)
         {
             return DB::table('items')
                 ->leftJoin('profits', 'items.typeID', '=', 'profits.typeID')
                 ->select('items.typeID', 'typeName', 'categoryName', 'metaGroupName', 'allowManufacture', 'manufactureCost', 'profitIndustry', 'profitImport', DB::raw('SUM(qty) AS qty'))
-                ->whereRaw('WHERE (' . implode(') and (', $whereraw) . ')')
+                ->whereRaw('(' . implode(') and (', $whereraw) . ')')
                 ->groupBy('typeID')
                 ->orderBy('qty', 'desc')
                 ->skip(($page - 1) * $per_page)
@@ -42,12 +42,14 @@ class Item extends Eloquent {
 
     public static function getRowCount($whereraw = NULL)
     {
-        $whererawsql = '';
-        if ($whereraw != NULL)
+        if (is_array($whereraw) && count($whereraw) > 0)
         {
-            $whererawsql = 'WHERE (' . implode(') and (', $whereraw) . ')';
+            $query = DB::select(DB::raw('SELECT COUNT(*) AS count FROM (SELECT typeID, SUM(qty) FROM items WHERE (' . implode(') and (', $whereraw) . ') GROUP BY typeID) AS custom'));
         }
-        $query = DB::select(DB::raw('SELECT COUNT(*) AS count FROM (SELECT typeID, SUM(qty) FROM items ' . $whererawsql . ' GROUP BY typeID) AS custom'));
+        else
+        {
+            $query = DB::select(DB::raw('SELECT COUNT(*) AS count FROM (SELECT typeID, SUM(qty) FROM items GROUP BY typeID) AS custom'));
+        }
         return $query[0]->count;
     }
 
