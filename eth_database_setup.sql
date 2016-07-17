71e9870f7ec33228c2f11fafb1c4e6eb43d16387
Matthew Pennell <github@matthewpennell.com>
2016-07-17 19:09:44 +0100

    Minor layout fixes.

--- eth_database_setup.sql
+++ eth_database_setup.sql
@@ -219,13 +219,14 @@ INSERT INTO `settings` (`id`, `key`, `label`, `value`, `created_at`, `updated_at
 --
 
 CREATE TABLE `fits` (
-`id` int(10) unsigned NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `eft_fitting` text COLLATE utf8_unicode_ci NOT NULL,
   `ship_dna` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
